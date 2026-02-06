a85ff4ce5007db32946254e872113b5ffb25db6a
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2026-02-06 23:27:10 +0400

    Check if passenger is visited using new Set to create groups

--- src/domain/special-category.ts
+++ src/domain/special-category.ts
@@ -1,4 +1,5 @@
-
+import type { Passenger } from "../types/passenger.js";
+import type { Group } from "../types/groups.js";
 
 // export function makeGroup(passengers: Passenger[]): Group[]  {
 // 1. Create a list called group
@@ -9,3 +10,24 @@
 // 6. iterate number 1 - 5 until we visit everyone
 // 7. return Group[]
 // }
+
+export function makeGroup(passengers: Passenger[]): Group[] {
+    const groups:Group[] = [];
+    const checked = new Set<string>();
+
+    for (const passenger of passengers) {
+        if(checked.has(passenger.id)) continue;
+
+        const members: string[] = [];
+        members.push(passenger.id);
+        // check group list. 
+        // if list? iterate the list and add each ids into group
+        // also do checked.add(passenger.id)for each 
+        // if not list, 바로 그룹
+
+        // groups.push ({id: , membersId: })
+
+    }
+    
+    return groups
+}
