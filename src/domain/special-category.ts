515bfd644a3bfa3d79888bec67bb936a20e061bc
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2026-02-06 23:05:45 +0400

    Create a basic Group interface

--- src/domain/special-category.ts
+++ src/domain/special-category.ts
@@ -1,4 +1,11 @@
-// write a function
-// getSpecialFlags
-// export function getSpecialFlags (p: Pax): SpecialFlags 
-// check what is true from boolean and return the flag.
+
+
+// export function makeGroup(passengers: Passenger[]): Group[]  {
+// 1. Create a list called group
+// 2. make a new group id (func)
+// 3. Add everyone traveling together with this person to this group (func)
+// 4. add this group to the group list
+// 5. mark them visited(func)
+// 6. iterate number 1 - 5 until we visit everyone
+// 7. return Group[]
+// }
---
+++ src/types/groups.ts
@@ -0,0 +1,6 @@
+
+export interface Group {
+    id: string,
+    membersId: string[],
+    size: number
+}
