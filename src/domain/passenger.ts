32b9018e41a6409550c2a5ebd1513403989738eb
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2026-02-07 18:38:01 +0400

    Function to build pax map by id

---
+++ src/domain/passenger.ts
@@ -0,0 +1,9 @@
+import type { PassengerWithFlags } from "../types/special.js";
+
+export function buildPassengersMapById(
+  passengers: PassengerWithFlags[],
+): Map<string, PassengerWithFlags> {
+  return new Map(
+    passengers.map((passenger) => [passenger.id, passenger] as const),
+  );
+}
