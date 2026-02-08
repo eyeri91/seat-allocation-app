cb052a1a6777d75c9e1d511acfa4e1c77a82468f
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2026-02-08 12:27:28 +0400

    Initialize assignUmnrGroups

--- src/rules/ruleUmnr.ts
+++ src/rules/ruleUmnr.ts
@@ -5,9 +5,33 @@
 import type { Group } from "../types/groups.js";
 import type { PassengerWithFlags } from "../types/special.js";
 import type { AssignedPassengerMap, SeatNumber } from "../types/seats.js";
-
+import type {
+  AssignSpecialData,
+  SpecialGroupAnchor,
+} from "../types/special.js";
 import { tryAssignSeatToPassenger } from "../domain/seatmap.utils.js";
 import { passengersWithFlags } from "../output/passengersWithFlags.js";
 import { getLeftSeatNumber, getRightSeatNumber } from "../utils/utils.js";
 import { getAssignedPassenger } from "../domain/seatmap.utils.js";
 import { generateAllSeatNumbers } from "../domain/seats.utils.js";
+import {
+  getAllSpecialGroups,
+  sortGroupsByNumbers,
+} from "../domain/special.utils.js";
+
+export function assignUmnrGroups({
+  groups,
+  passengersByIds,
+  aisleSeatNumbers,
+  assignedPassengerMap,
+}: AssignSpecialData<"hasUMNR", "isUMNR">): SpecialGroupAnchor[] {
+  const results: SpecialGroupAnchor[] = [];
+  const umnrGroups = getAllSpecialGroups(groups, "hasUMNR");
+  const sortedUmnrGroupsNumbers = sortGroupsByNumbers(
+    groups,
+    passengersByIds,
+    "isUMNR",
+  );
+  console.log(umnrGroups);
+  return results;
+}
